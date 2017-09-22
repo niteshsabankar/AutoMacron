bdd5ad44b71379e5a17f6640d4fcb8d682f672bb
ZerglingOne <jholcomb87@gmail.com>
2017-09-22 04:38:35 -0700

    Add files via upload

---
+++ PlaybackMacro.cpp
@@ -0,0 +1,15 @@
+#include "PlaybackMacro.h"
+
+void PlaybackMacro::actionLoop()
+{
+	//do all of the recorded things.
+	//go through the vector, perform actions and sleep for delays
+
+}
+
+void PlaybackMacro::loadMacro(string name)
+{
+	//SHARED WITH EDITMACRO!!!!
+	//look for file named name + ".mcr" in \macros\
+	//load and ask user to start (or press start macro button on the GUI when it's a GUI.)
+}
---
+++ RecordMacro.cpp
@@ -0,0 +1,28 @@
+#include "RecordMacro.h"
+
+RecordMacro::RecordMacro(string name)
+{
+	//interface will ask user for the name.
+	macro_name = name + ".mcr";
+}
+
+vector<int> RecordMacro::recordMacro(int mode)
+{
+	//NOTE: this code section is to be shared with the EditMacro file
+	//loop until scroll lock is hit VK_SCROLL/0x91
+	//if mode is 1, delays are recorded
+	//if mode is 2, delays are not recorded
+	//basically while it's waiting between keys, it will count the milliseconds
+	//with a native time function. once a key is pressed, it will record the delay
+	//and append it to the vector with push_back along with the new event;
+	//if in mode 2, push a 0 between each event into the vector.
+}
+
+RecordMacro::saveMacro()
+{
+	//SHARED CODE FROM EDITMACRO
+	////open a file named \macros\macro_name in write mode
+	//for(int i =0; i < actions.size(); i++)
+	//file << actions[i]
+	//file << ',' or ' ';
+}
