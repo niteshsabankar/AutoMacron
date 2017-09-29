6658656c50c8b71057b9f3f0900b58b0c243888b
Ares250 <32182870+Ares250@users.noreply.github.com>
2017-09-28 17:27:47 -0700

    Update PlaybackMacro.cpp

--- PlaybackMacro.cpp
+++ PlaybackMacro.cpp
@@ -6,6 +6,19 @@ void PlaybackMacro::PlaybackMacro(string load_me)
 }
 void PlaybackMacro::actionLoop()
 {
+	if (NULL == p)
+	{return;}
+
+	while (*p)
+	{
+	printf("%c\xDB", *p++);     // %c , *p++type one char at a time
+									//\xDB cursor sim
+	::Sleep(60);                // minal secs.
+	printf("\b \b");            // cursor blink \b moves cursor one space pback  then space move cirsor back 
+	::Sleep(60);
+	}
+	
+	::Sleep(300); 
 	//do all of the recorded things.
 	//go through the vector, perform actions and sleep for delays
 
@@ -13,6 +26,23 @@ void PlaybackMacro::actionLoop()
 
 void PlaybackMacro::loadMacro(string name)
 {
+	char start;
+	printf("PlayBack:         ");
+	cin >> start;
+
+	if (start == 'p' || start == 'P')
+	{
+		printf("\nBeginning playback\n");
+		::Sleep(3000);
+
+		const char* tmp = name.c_str();
+		actionLoop(tmp);
+	}
+	else
+	{
+		printf("Exiting...");
+		::Sleep(3000);
+	}
 	//SHARED WITH EDITMACRO!!!!
 	//look for file named name + ".mcr" in \macros\
 	//load and ask user to start (or press start macro button on the GUI when it's a GUI.)
