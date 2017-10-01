569b5183397bad9a9514776a4c79318fe66edf32
Ares250 <32182870+Ares250@users.noreply.github.com>
2017-10-01 01:17:47 -0700

    Update PlaybackMacro.h

--- PlaybackMacro.h
+++ PlaybackMacro.h
@@ -1,15 +1,38 @@
+//=========================================================
+//NAME: Omar Gonzalez Martinez
+//DATE: 10/1/17
+//Compiler:
+//FILE: PlaybackMarco header file
+//=========================================================
 #ifndef PLAYBACKMACRO_H
 #define PLAYBACKMACRO_H
 
+//--------Globally setting up the aliases------------------
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <Windows.h>
+#include <vector>
+
+using namespace std;
+
 class PlaybackMacro : public Macro
 {
-	public:
-		PlaybackMacro(string);
-		void loadMacro(string);
-		void actionLoop();
-
+private:
+    vector<int> actions; //
+    
+public:     //prototypes to be used by client
+    
+    PlaybackMacro(wstring);         //Constructor
+    
+    //PURPOSE: Loads file contexts into vector
+    //PARAMETER: Wstring tpye is the name of the file
+    void loadMacro(wstring);
+    
+    //PURPOSE: Plays vector context
+    void actionLoop();
 };
 
 #endif // !PLAYBACKMACRO_H
-
 
