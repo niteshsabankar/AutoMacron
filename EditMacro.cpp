1a04bffa0b3e3e8bc18d1189696c87ca782f16dd
Nitesh sabankar <niteshsabankar@gmail.com>
2017-10-05 16:20:20 -0700

    edited saveMacro and recordMacro

--- EditMacro.cpp
+++ EditMacro.cpp
@@ -19,6 +19,16 @@ EditMacro::saveMacro()
 	//for(int i =0; i < actions.size(); i++)
 	//file << actions[i]
 	//file << ',';
+	
+	ofstream outputFile;
+	outputFile.open("macro_name");
+
+	for (int i = 0; i < actions.size(); i++)
+	{
+		outputFile << actions[i] << " " << endl;
+	}
+	outputFile.close();
+	
 }
 
 void EditMacro::loadMacro(wstring file_name)
@@ -38,6 +48,49 @@ void EditMacro::record(int mode)
 	//with a native time function. once a key is pressed, it will record the delay
 	//and append it to the vector with push_back along with the new event;
 	//if in mode 2, push a 0 between each event into the vector.
+	
+	///////////////////////////////////////////////////////////Keyboard/////////////////////////////////////////////////////////////////////	
+	if(mode==1)		//With Delays
+	{	
+		int delay = 0;
+		time_t end = 0; 
+			while (true)
+			{
+				for (int key = 8; key <= 255; key++)
+				{
+					if (GetAsyncKeyState(key) == -32767)
+					{		
+					time_t start = time(0);
+					actions.push_back(key);
+					delay = start-end;
+					cout << key << " " << delay << endl;
+					actions.push_back(delay);
+					end = time(0);
+					}
+				}
+			}
+	}
+	
+	else if(mode==2)	//No Delays
+	{	
+		while (true)
+		{
+			for(int key=8; key<=255; key++)
+			{ 
+				if (GetAsyncKeyState(key) == -32767)
+				{	
+					actions.push_back(key);
+					actions.push_back(0);
+				}
+			}
+		}
+	}
+	
+	else
+		return 0;
+///////////////////////////////////////////////////////////////Mouse/////////////////////////////////////////////////////////////////////
+	
+	
 }
 
 void EditMacro::recordSingle(int mode)
