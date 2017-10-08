bfda7ee1f3db00228b9b8dfa8bdbda73d85f2656
John Romanski <32182484+Romanski22@users.noreply.github.com>
2017-10-08 16:03:47 -0700

    Update RecordMacro.cpp

--- RecordMacro.cpp
+++ RecordMacro.cpp
@@ -79,8 +79,6 @@ vector<int> RecordMacro::recordMacro(int mode)
 		return 0;
 ///////////////////////////////////////////////////////////////Mouse/////////////////////////////////////////////////////////////////////
 	
-	
-	
 	//NOTE: this code section is to be shared with the EditMacro file
 	//loop until scroll lock is hit VK_SCROLL/0x91
 	//if mode is 1, delays are recorded
@@ -93,12 +91,34 @@ vector<int> RecordMacro::recordMacro(int mode)
 
 RecordMacro::saveMacro()
 {
+	vector<char>hex;
+	long int remainder, quotient;
+	int temp;
+
 	ofstream outputFile;
 	outputFile.open("macro_name ");
 
 	for (int i = 0; i < actions.size(); i++)
 	{
-		outputFile << actions[i] << "," << endl;
+		quotient = actions[i];
+
+		while (quotient != 0)
+		{
+			temp = quotient % 16;
+
+			if (temp < 10)
+				temp = temp + 48;
+			else
+				temp = temp + 55;
+				hex.insert(hex.begin(), temp);
+				quotient = quotient / 16;
+		}
+		for (int i = 0; i <hex.size(); i++)
+		{
+			outputFile << hex[i];
+		}
+		outputFile << " ";
+		hex.clear();
 	}
 	outputFile.close();
 	
@@ -117,100 +137,4 @@ RecordMacro::pause()
 RecordMacro::resume()
 {
 	
-}
-
-///////////////////////////////////////////////////////////////Initial example/////////////////////////////////////////////////////////////////////////
-#include <Windows.h>
-#include <stdio.h>
-#include <iostream>
-using namespace std;
-
-const int MacroMaxLen = 100;
-
-struct Macro
-{
-	int SleepTime;						//How long to wait 
-	int SleepTimeBeforeClick;				//How long to wait before click
-	int MacroIndex;						//Position in Array
-	POINT CursorPosition[MacroMaxLen];			//Array that holds x,y position of cursor
-};
-
-void MouseLeftClick()
-{
-	INPUT Input = { 0 };					//
-	Input.type = INPUT_MOUSE;				//
-	Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;		//
-	SendInput(1, &Input, sizeof(INPUT));			//
-
-	ZeroMemory(&Input, sizeof(INPUT));			//
-	Input.type = INPUT_MOUSE;				//
-	Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;			//
-	SendInput(1, &Input, sizeof(INPUT));			//
-}
-
-void IsertNewMacroPos(Macro* macro)
-{
-	if (macro->MacroIndex + 1 < MacroMaxLen)
-		{
-		macro->MacroIndex++;
-		GetCursorPos(&macro->CursorPosition[macro->MacroIndex]);
-		cout << "New Macro Inserted" << endl;
-		}
-}
-
-void ExecuteMacro(Macro* macro)
-{
-	for (int i = 0; i < macro->MacroIndex + 1; i++)
-	{
-		SetCursorPos(macro->CursorPosition[i].x, macro->CursorPosition[i].y);
-		Sleep(macro->SleepTimeBeforeClick);	
-		MouseLeftClick();
-		Sleep(macro->SleepTime);
-	}
-}
-
-Macro CreateNewMacro(int sleepbeforeclick, int sleep)
-{
-	Macro macro;
-	macro.SleepTime = sleep;
-	macro.SleepTimeBeforeClick = sleepbeforeclick;
-	macro.MacroIndex = -1;
-	return macro;
-
-}
-
-
-int main()
-{
-	bool macroInsertDone = true;
-	bool macroExecuteDone = true;
-
-	Macro m = CreateNewMacro(50, 50);
-
-	while (true)
-	{
-		//Set new macro position
-		if (GetAsyncKeyState('Z'))
-			macroInsertDone = false;
-		else if (!GetAsyncKeyState('Z'))
-		{
-			if (!macroInsertDone)
-				IsertNewMacroPos(&m);
-			macroInsertDone = true;	
-		}
-		//Execute macro
-		if (GetAsyncKeyState('X'))
-			macroExecuteDone = false;
-		else if (!GetAsyncKeyState('X'))
-		{
-			if (!macroExecuteDone)
-				ExecuteMacro(&m);
-			macroExecuteDone = true;
-		}
-		Sleep(5);
-	}
-
-
-
-	return 0;
 }
