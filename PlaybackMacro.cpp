fc2a8d7dd03f2c2119645a57cf0cf464c7ca4085
Ares250 <32182870+Ares250@users.noreply.github.com>
2017-10-12 17:28:52 -0700

    Update PlaybackMacro.cpp

--- PlaybackMacro.cpp
+++ PlaybackMacro.cpp
@@ -12,78 +12,98 @@
 //AlGORITHM: Use LoadMacro() function
 PlaybackMacro::PlaybackMacro(wstring load_me)
 {
-    loadMacro(load_me);
+	loadMacro(load_me);
 }
 
-//PURPOSE:
-//PARAMETER:
-//AlGORITHM:
 void PlaybackMacro::actionLoop()
 {
-    int code;
-    INPUT key;
-    
-    reverse(actions.begin(), actions.end());
-    ::Sleep(5000);
-    
-    while (!actions.empty())
-    {
-        ::Sleep(500)
-        code = actions.front();
-        actions.pop_back();
-        
-        key.type = INPUT_KEYBOARD;
-        key.ki.wScan = 0;
-        key.ki.time = 0;
-        key.ki.dwExtraInfo = 0;
-        
-        key.ki.wVk = code;
-        key.ki.dwFlags = 0;
-        SendInput(1, &key, sizeof(INPUT));
-        
-        key.ki.dwFlags = KEYEVENTF_KEYUP;
-        SendInput(1, &key, sizeof(INPUT));
-    }
-}
+	int code;
+	bool flag = false;
 
-//PURPOSE:
-//PARAMETER:
-//AlGORITHM:
+	INPUT key;
+	::Sleep(5000);
+    
+	for (int i = 0; i < actions.size(); i++)
+	{
+		if (i % 2 == 0)
+		{
+			clock_t wait;
+			wait = clock() + (((float)actions[i] / 1000.0) * CLOCKS_PER_SEC);
+			while (clock() < wait);
+		}
+		else
+		{
+			code = actions[i];
+			if (code >= 1000)
+			{
+				code -= 1000;
+				flag = true;
+			}
+			else
+			{flag = false;}
+
+			key.type = INPUT_KEYBOARD;
+			key.ki.wScan = 0;
+			key.ki.time = 0;
+			key.ki.dwExtraInfo = 0;
+
+			key.ki.wVk = code;
+			
+			if (flag)
+			{
+				key.ki.dwFlags = KEYEVENTF_KEYUP;
+				SendInput(1, &key, sizeof(INPUT));
+			}
+			else
+			{
+				key.ki.dwFlags = 1;//0
+				SendInput(1, &key, sizeof(INPUT));
+			}
+			
+		}
+	}
+}
+		
 void PlaybackMacro::loadMacro(wstring name)
 {
-    fstream file;
-    file.open(name);
-    
-    if (!file)
-    {
-        wcout << name << " File not found" << endl;
-        exit(1);
-    }
-    else
-    {
-        string context;
-        string tmp_hex;
-        int num;
-        
-        file >> context;
-        
-        for (int i = 0; i < context.length(); i++)
-        {
-            if (context[i] != ',')
-            {
-                tmp_hex += context[i];
-            }
-            else if(context[i] == ',')
-            {        
-                const char* hex = tmp_hex.c_str();
-                num = (int)strtol(hex, NULL, 16);       // number base 16
-                
-                actions.push_back(num);
-                tmp_hex = "\0";
-            }
-        }
-        file.close();
-        
-        actionLoop();	
-    }
+	fstream file;
+	file.open(name);
+
+	if (!file)
+	{
+		wcout << name << " File not found" << endl;
+		exit(1);
+	}
+	else
+	{
+		string context;
+		string tmp;
+		int num;
+
+		file >> context;
+		while (file)
+		{
+			for (int i = 0; i < context.length(); i++)
+			{
+				if (context[i] != ',')
+				{
+					tmp += context[i];
+				}
+				else if (context[i] == ',')
+				{
+					const char* dec = tmp.c_str();
+					num = (int)strtol(dec, NULL, 10);       // number base 16
+
+					actions.push_back(num);
+					
+
+					tmp = "\0";
+				}
+			}
+			file >> context;
+		}
+		file.close();
+
+		actionLoop();	
+	}
 }
