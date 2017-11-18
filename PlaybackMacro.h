bd3fcf297740d12207749c4cb1d927db87df276e
Ares250 <32182870+Ares250@users.noreply.github.com>
2017-11-18 14:43:51 -0800

    Update PlaybackMacro.h

--- PlaybackMacro.h
+++ PlaybackMacro.h
@@ -8,80 +8,43 @@
 #define PLAYBACKMACRO_H
 
 //--------Globally setting up the aliases------------------
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <Windows.h>
-#include <vector>
-
-using namespace std;
-
-class PlaybackMacro : public Macro
-{
-private:
-    vector<int> actions; //
-    
-public:     //prototypes to be used by client
-    
-    PlaybackMacro(wstring);         //Constructor
-    
-    //PURPOSE: Loads file contexts into vector
-    //PARAMETER: Wstring tpye is the name of the file
-    void loadMacro(wstring);
-    
-    //PURPOSE: Plays vector context
-    void actionLoop();
-};
-
-#endif // !PLAYBACKMACRO_H
-
-//With mouse movement
-//=========================================================
-//NAME: Omar Gonzalez Martinez
-//DATE: 10/1/17
-//Compiler:
-//FILE: PlaybackMarco header file
-//=========================================================
-#ifndef PLAYBACKMACRO_H
-#define PLAYBACKMACRO_H
-#pragma once
-
-//--------Globally setting up the aliases------------------
-//#include "Macro.h"  all #include will be in Macro.h
-#include <fstream>
+#include <fstream> //All includes will be in the macro.h
 #include <iostream>
 #include <string>
 #include <stdio.h>
 #include <Windows.h>
 #include <vector>
 #include <time.h>
-//--------------------------------------------------------------
+
 using namespace std;
 
-class PlaybackMarco
+class PlaybackMacro
 {
-	//actions will be in Macro.h
 private:
-	vector<int> actions;
+	vector<int> actions; //These will be in the marco.h
 	vector<long> mouseX;
 	vector<long> mouseY;
 
-
 public:     //prototypes to be used by client
 
-	PlaybackMarco(wstring);         //Constructor
+	PlaybackMacro(wstring);         //Constructor
 
-									//PURPOSE: Loads file contexts into vector
-									//PARAMETER: Wstring tpye is the name of the file
+	//PURPOSE: Loads file contexts into vector
+	//PARAMETER: Wstring tpye is the name of the file
 	void loadMacro(wstring);
 
 	//PURPOSE: Plays vector context
-
 	void actionLoop();
 
-	void actionKey(int);
+	//PURPOSE: Play a key event
+	//PARAMETER: One int type - the index of the vector
+	void actionKey(int, int&);
 
-	void actionMouse(int);
+	//PURPOSE: Play a mouse event
+	//PARAMETER: One int type - the index of the vector
+	void actionMouse(int, int&);
+
+	
 };
+
 #endif // !PLAYBACKMACRO_H
