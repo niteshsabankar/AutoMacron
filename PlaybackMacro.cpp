eed417d9f0ad92c265951f9892e587bf658d229b
Ares250 <32182870+Ares250@users.noreply.github.com>
2017-11-18 14:42:31 -0800

    Update PlaybackMacro.cpp

--- PlaybackMacro.cpp
+++ PlaybackMacro.cpp
@@ -1,133 +1,98 @@
-//===============================================================
-//NAME: Omar Gonzalez Martinez
-//Date: 10/1/17
-//Complier:
-//File type: PlaybackMarcro implementaion file
-//===============================================================
 #include "stdafx.h"
 #include "PlaybackMacro.h"
 
-//PURPOSE: Constructor initialize
-//PARAMETER: wstring varible is the name of the file being loaded
-//AlGORITHM: Use LoadMacro() function
+//PURPOSE: Constructor
+//PARAMETER: wstring file name being upload
 PlaybackMacro::PlaybackMacro(wstring load_me)
 {
 	loadMacro(load_me);
 }
 
-void PlaybackMacro::actionLoop()
-{
-	int code;
-	bool flag = false;
-
-	INPUT key;
-	::Sleep(5000);
-    
-	for (int i = 0; i < actions.size(); i++)
-	{
-		if (i % 2 == 0)
-		{
-			clock_t wait;
-			wait = clock() + (((float)actions[i] / 1000.0) * CLOCKS_PER_SEC);
-			while (clock() < wait);
-		}
-		else
-		{
-			code = actions[i];
-			if (code >= 1000)
-			{
-				code -= 1000;
-				flag = true;
-			}
-			else
-			{flag = false;}
-
-			key.type = INPUT_KEYBOARD;
-			key.ki.wScan = 0;
-			key.ki.time = 0;
-			key.ki.dwExtraInfo = 0;
-
-			key.ki.wVk = code;
-			
-			if (flag)
-			{
-				key.ki.dwFlags = KEYEVENTF_KEYUP;
-				SendInput(1, &key, sizeof(INPUT));
-			}
-			else
-			{
-				key.ki.dwFlags = 1;//0
-				SendInput(1, &key, sizeof(INPUT));
-			}
-			
-		}
-	}
-}
-		
+//PURPOSE: Load the file content in the vectors
+//PARAMETER: wstring( name of the file being uploaded
+//AlGORITHM: if does not file exist 
+//		else countine with the uplaod with for loop
 void PlaybackMacro::loadMacro(wstring name)
 {
 	fstream file;
-	file.open(name);
+	file.open(name.c_str());
 
-	if (!file)
+	if (!file)	//Error file does not exist exit program
 	{
 		wcout << name << " File not found" << endl;
 		exit(1);
 	}
 	else
 	{
-		string context;
-		string tmp;
-		int num;
+		//-------Local variables------------------------------------------------------------------------
+		string context;      //Holds one content of the file
+		string tmp_num;      //Holds one char of the file
+		int flag;            //Flag for which vector to input
+		int num;             //Convet string input to int
 
-		file >> context;
-		while (file)
+		string::size_type sz;
+
+		while (file >> context)
 		{
+			if (context == "K")  //Flag one key stroke input
+			{
+				flag = 1;
+				file >> context;
+			}
+
+			else if (context == "MX")  //Flag 2 mouse X point
+			{
+				flag = 2;
+				file >> context;
+			}
+
+			else if (context == "MY")  //Flag 3 mouse Y point
+			{
+				flag = 3;
+				file >> context;
+			}
+
+			//For go throuht each char in the file if char add to context if "," add to vector
+			//--------------------------------------------------------------------
 			for (int i = 0; i < context.length(); i++)
 			{
 				if (context[i] != ',')
-				{
-					tmp += context[i];
-				}
+					tmp_num += context[i];
+
 				else if (context[i] == ',')
 				{
-					const char* dec = tmp.c_str();
-					num = (int)strtol(dec, NULL, 10);       // number base 16
+					num = stoi(tmp_num, &sz);    // number base 16
 
-					actions.push_back(num);
-					
+					if (flag == 1)
+						actions.push_back(num);
 
-					tmp = "\0";
+					else if (flag == 2)
+						mouseX.push_back((long)num);
+
+					else if (flag == 3)
+						mouseY.push_back((long)num);
+
+					tmp_num = "\0";   //Restart context
 				}
 			}
-			file >> context;
 		}
-		file.close();
-
-		actionLoop();	
 	}
-}
-//====================================================================================
-//with mouse movements
-#include "stdafx.h"
-#include "PlaybackMarco.h"
-
-using namespace std;
-//PURPOSE: Constructor initialize
-//PARAMETER: wstring varible is the name of the file being loaded
-//AlGORITHM: Use LoadMacro() function
-
-PlaybackMacro::PlaybackMacro(wstring load_me)
-{
-	loadMacro(load_me);
+	file.close();  //Close file
 }
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//PURPOSE: Play key and mouse actions until no more events
+//AlGORITHM: While loop coutines until mouse and key vector has no events
+//     if Key delay do as many mouse movements and delays possible within given time
+//        else normal key input
+//     if Mouse delay do as many key inputs and dealys as possible with goven time 
+//        else normal mouse movement
 void PlaybackMacro::actionLoop()
 {
-	clock_t startKey = 0, endKey = 0, startMouse, endMouse;
-	int timeDif = 0, hold = 0;
-	int i = 0, x = 0;
+	//-----------Local variables---------------------------------------------------
+	clock_t start, end;
+	int timeDif = 0, hold = 0, timeReturn = 0;
+	int i = 0, x = 0, howMany = 0;
+	bool flag;
 
 	::Sleep(5000);
 
@@ -135,69 +100,182 @@ void PlaybackMacro::actionLoop()
 	{
 		if ((i >= actions.size()) && (x >= mouseX.size()))
 		{
-			cout << "done" << endl;
 			break;
 		}
 
-		if (actions.size() < i)
-		{
-			cout << "key is done" << endl;
-		}
-
-		if (mouseX.size() < x)
-
-		{
-
-			cout << "mouse is done" << endl;
-
-		}
-
+		flag = false;
 		//Key vector is still has content
-
-		//========================================================================================
-
+		//==============================================================================================
 		if (actions.size() > i)
 		{
-
-			cout << "In key input" << endl;
-
 			//This is a key delay
-			//-------------------------------------------------------------------------------
+			//------------------------------------------------------------------------------------------
 			if ((i % 2) == 0)
 			{
-				cout << "Key delay" << endl;
 				//key delay- will do as many mouse movement as it can within the deley time
-				//-------------------------------------------------------------------------------
-				if (mouseX.size() > x)
+				//--------------------------------------------------------------------------------------
+				if (mouseX.size() > x) 
 				{
-					cout << "first movement of key delay" << endl;
-					startMouse = clock();
-					actionMouse(x);
-					endMouse = clock();
-
-					x++;
-
-					timeDif = endMouse - startMouse;
-
-					if (timeDif <= actions[i])
+					//Next mouse event is a delay, but delay time is bigger than key delay
+					//--------------------------------------------------------------------------------
+					if ((mouseX[x] >= actions[i]) && ((x % 2) != 0))
 					{
-						hold = actions[i] - timeDif;
+						mouseX[x] = mouseX[x] - actions[i];
+						actionKey(i, timeDif);
+						i++; 
+					}
+					//-------------Mouse event was a delay that was to big----------------------------
+
+					//Next mouse event is a either delay or event that can be done within given key delay time
+					//----------------------------------------------------------------------------------------
+					else
+					{
+						actionMouse(x, timeDif);
+						x++;
+
+						//timeDif allows for more events to be done during this key delay
+						//------------------------------------------------------------------------------------
+						if (timeDif < actions[i])
+						{
+							hold = actions[i] - timeDif;
+
+							i++;
+							timeDif = 0;
+
+							//while loop do as many evenst
+							//---------------------------------------------------------------------------------
+							while (hold > timeDif)
+							{
+								start = clock();
+								//Still events in the mouse vector
+								//--------------------------------------------------------------------------
+								if (mouseX.size() > x)
+								{
+									
+									if ((mouseX[x] >= hold) && ((x % 2) != 0)) //
+									{
+										mouseX[x] = mouseX[x] - timeDif;
+										timeReturn = mouseX[x];
+										::Sleep(hold);
+										x--;
+									}
+									else
+									{
+										actionMouse(x, timeReturn);
+									}
+									x++;
+									timeDif += timeReturn;
+								}
+								//----End of event---------------------------------------------------------
+
+								//No more mouse event
+								//--------------------------------------------------------------------------
+								else
+								{
+									end = clock();
+									timeDif = timeDif +((int)end - (int)start);
+								}
+								//----Stay in while loop unitl delay is over-------------------------------
+							}
+							//---End of while loop ------------------------------------------------------------
+						}
+						//---------------End of mouse events within given delay--------------------------------
+					}
+					//------------------End of key delay with X number of mouse events done --------------------
+				}
+				//----------------------End of key delay with possible mouse movements--------------------------
+
+				//No more mouse movements just key delays
+				//-------------------------------------------------------------------------------------
+				else
+				{
+					actionKey(i, timeDif);
+					i++;
+				}
+				//----------------------End of Key delay without mouse movement------------------------		
+			}
+			//-------End of key delay-------------------------------------------------------------------
+
+			//------------------------------------------------------------------------------------------
+			//not a delay do key action
+			else
+			{
+				actionKey(i, timeDif);
+				i++;
+			}
+			//-------End of key action------------------------------------------------------------------
+		}
+		//End of Key event
+		//===============================================================================================
+
+		//Mouse vector still has content
+		//===============================================================================================
+		if (mouseX.size() > x)
+		{
+			//This is a mouse delay
+			//-------------------------------------------------------------------------------
+			if ((x % 2) != 0)
+			{
+				//mouse delay- will do as many key inputs as it can within the deley time
+				//-------------------------------------------------------------------------------
+				if (actions.size() > i)
+				{
+					//Next key event is a delay, but delay time is bigger than mouse delay
+					if ((actions[i] >= mouseX[x]) && ((i % 2) == 0))
+					{
+						actions[i] = actions[i] - mouseX[x];
+						actionMouse(x, timeDif);
+						x++;
+					}
+
+					//Next key event is a either delay or event that can be done within given mouse delay time
+					else
+					{
+						actionKey(i, timeDif);
 						i++;
 
-						timeDif = 0;
-						while (hold >= timeDif)
+						//timeDif allows for more events to be done during this mouse delay
+						if (timeDif < mouseX[x])
 						{
-							startMouse = clock();
-							if (mouseX.size() > x)
+							hold = mouseX[x] - timeDif;
+							x++;
+							timeDif = 0;
+							//start of while loop
+							//-----------------------------------------------------------------------------
+							while (hold >= timeDif)
 							{
-								cout << "mouse vector number " << x << endl;
-								actionMouse(x);
-								x++;
+								start = clock();
+								//Still events in the mouse vector
+								//--------------------------------------------------------------------------
+								if (actions.size() > i)
+								{
+
+									if ((actions[i] >= hold) && ((i % 2) == 0)) //
+									{
+										actions[i] = actions[i] - timeDif;
+										timeReturn = actions[i];
+										::Sleep(hold);
+										i--;
+									}
+									else
+									{
+										actionKey(i, timeReturn);
+									}
+									i++;
+									timeDif += timeReturn;
+								}
+								//----End of event---------------------------------------------------------
+
+								//No more mouse event
+								//--------------------------------------------------------------------------
+								else
+								{
+									end = clock();
+									timeDif = timeDif + ((int)end - (int)start);
+								}
+								//----Stay in while loop unitl delay is over-------------------------------
 							}
-
-							endMouse = clock();
-
-							timeDif = timeDif + (endMouse - startMouse);
+							//---End of while loop ------------------------------------------------------------
 						}
 					}
 				}
@@ -206,89 +284,18 @@ void PlaybackMacro::actionLoop()
 				//No more mouse movements just key delays
 				else
 				{
-					cout << "no mouse movements just key delays" << endl;
-					actionKey(i);
-					i++;
+					actionMouse(x, timeDif);
+					x++;
 				}
 			}
 			//not a delay do key action
 			else
 			{
-				cout << "not a delay" << endl;
-				actionKey(i);
-				i++;
-			}
-
-			cout << "Out of key input" << endl;
-		}
-		//====================================================================================
-
-		//Mouse vector still has content
-		//====================================================================================
-		if (mouseX.size() > x)
-		{
-			cout << "In side mouse vetcor " << x << endl;
-			cout << mouseX[x] << " " << mouseY[x] << endl;
-
-			//This is a mouse delay
-			if ((x % 2) != 0)
-			{
-				cout << "This is a muouse delay" << endl;
-
-				//Mouse delay - will do as many key input within the delay time
-				//---------------------------------------------------------------------------------
-				if (actions.size() > i)
-				{
-					startKey = clock();
-					actionKey(i);
-					endKey = clock();
-
-					i++;
-
-					timeDif = endKey - startKey;
-
-					if (timeDif <= mouseX[x])
-					{
-						hold = mouseX[x] - timeDif;
-						x++;
-
-						timeDif = 0;
-
-						while (hold >= timeDif)
-						{
-							startKey = clock();
-							if (actions.size() > i)
-							{
-								cout << "Stuck" << endl;
-								actionKey(i);
-								i++;
-							}
-							endKey = clock();
-
-							timeDif = timeDif + (endKey - startKey);
-						}
-					}
-				}
-				//---------------------------------------------------------------------------------
-				else
-				{
-					cout << "in delay without key " << endl;
-					actionMouse(x);
-
-					x++;
-				}
-			}
-
-			//not a delay do mouse action
-			else
-			{
-				cout << "not delay" << endl;
-				actionMouse(x);
+				actionMouse(x, timeDif);
 				x++;
 			}
 		}
-		//=====================================================================================
-
+		//====================================================================================
 	}
 }
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -296,58 +303,16 @@ void PlaybackMacro::actionLoop()
 //PURPOSE:
 //PARAMETER:
 //AlGORITHM:
-void PlaybackMacro::actionKey(int i)
+void PlaybackMacro::actionKey(int i, int& timeDif)
 {
 	INPUT key;
 	bool flag = false;
-	//+++++++++++++++++++++++++++++++++++++++++++++
-	/*
-	bool mouseFlag = false;
-	int del = (int) clock();
-	int mouseIndex = 1;
-	*/
-	//+++++++++++++++++++++++++++++++++++++++++++++
+	clock_t start, end;
 
-	//for (int i = 0; i < actions.size(); i++)
-	//{
+	start = clock();
+
 	if (i % 2 == 0)
-	{
 		::Sleep(actions[i]);
-
-		/*
-		THIS DID NOT WORK AND I COULD NOT FIX
-		::Sleep(actions[i]);
-		int hold = (int)clock();
-
-		cout << " actions[i] " << actions[i] << endl;
-		cout << "del " << del << endl;
-		cout << "+ " << actions[i] + del << endl;
-		cout << " Hold " << (int)clock() << endl;
-
-		while ((int)clock() < actions[i] + del)
-		{
-		cout << "In while" << endl;
-
-		int mouseDel = mouseX[mouseIndex];
-
-		if (((int)clock() == (hold + mouseDel)) && (!mouseFlag))
-		{
-		cout << "move" << endl;
-
-		mouseIndex++;
-		actionMouse(mouseIndex);
-		mouseIndex++;
-
-		mouseFlag = true;
-		}
-
-		if ((int)clock() != (hold + mouseDel) && mouseFlag)
-		{
-		mouseFlag = false;
-		}
-		}
-		*/
-	}
 
 	else
 	{
@@ -356,7 +321,7 @@ void PlaybackMacro::actionKey(int i)
 
 		else
 			flag = false;
-
+			
 		key.type = INPUT_KEYBOARD;
 		key.ki.wScan = 0;
 		key.ki.time = 0;
@@ -381,7 +346,6 @@ void PlaybackMacro::actionKey(int i)
 			while (x >= 0)
 			{
 				//key.ki.dwFlags = KEYEVENTF_KEYUP;
-
 				while (print >= 0)
 				{
 					::Sleep(50);
@@ -391,33 +355,35 @@ void PlaybackMacro::actionKey(int i)
 				x--;
 			}
 		}
-
+		
 		else
 		{
 			key.ki.wVk = actions[i];
 			key.ki.dwFlags = 0;
-
+			
 			SendInput(1, &key, sizeof(INPUT));
 		}
-
-		/*
-		if ((i + 1) != actions.size())
-		{
-		del = actions[i + 1];
-		}
-		*/
 	}
-	//} for loop end
+
+	end = clock();
+	timeDif = (int)end - (int)start;
 }
 
-
-void PlaybackMacro::actionMouse(int i)
+//PURPOSE:
+//PARAMETER:
+//AlGORITHM:
+void PlaybackMacro::actionMouse(int i, int& timeDif)
 {
 	INPUT key;
 	POINT positions;
+	clock_t start, end;
+
+	start = clock();
 
 	if (i % 2 != 0)
+	{
 		::Sleep(mouseX[i]);
+	}
 
 	else
 	{
@@ -431,75 +397,7 @@ void PlaybackMacro::actionMouse(int i)
 
 		SetCursorPos(positions.x, positions.y);
 	}
-}
+	end = clock();
+	timeDif =(int)end - (int)start;
 
-//PURPOSE:
-//PARAMETER:
-//AlGORITHM:
-void PlaybackMacro::loadMacro(wstring name)
-{
-	fstream file;
-	file.open(name.c_str());
-
-	if (!file)
-	{
-		wcout << name << " File not found" << endl;
-		exit(1);
-	}
-
-	else
-	{
-		string context;
-		string tmp_hex;
-
-		int flag;
-		int num;
-
-		string::size_type sz;
-
-		while (file >> context)
-		{
-			if (context == "K")
-			{
-				flag = 1;
-				file >> context;
-			}
-
-			else if (context == "MX")
-			{
-				flag = 2;
-				file >> context;
-			}
-
-			else if (context == "MY")
-			{
-				flag = 3;
-				file >> context;
-			}
-
-			for (int i = 0; i < context.length(); i++)
-			{
-				if (context[i] != ',')
-					tmp_hex += context[i];
-
-				else if (context[i] == ',')
-				{
-					num = stoi(tmp_hex, &sz);    // number base 16
-
-					if (flag == 1)
-						actions.push_back(num);
-
-					else if (flag == 2)
-						mouseX.push_back((long)num);
-
-					else if (flag == 3)
-						mouseY.push_back((long)num);
-
-					tmp_hex = "\0";
-				}
-			}
-		}
-		actionLoop(); //remove
-	}
-	file.close();
 }
