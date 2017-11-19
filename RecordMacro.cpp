c32e44d992d8941056583b9606818036fd302ee3
John Romanski <32182484+Romanski22@users.noreply.github.com>
2017-11-19 09:43:14 -0800

    Update RecordMacro.cpp

--- RecordMacro.cpp
+++ RecordMacro.cpp
@@ -14,63 +14,89 @@ RecordMacro::RecordMacro(string name)
 vector<int> RecordMacro::recordMacro(int mode)
 {
 	bool flags[256];				//Flag for every key
+	bool tic = false;				//Flag to check if MousePos happened in clock tick
+	bool noMouse = false;				//If user doesn't want Mouse positions
+	bool noKey = false;				//If user doesn't want Key delays
 	int delay = 0;					//Time delay between key events
-	clock_t timer = clock();			//
-	clock_t timer2 = clock();			//
-	
-	for (int i = 0; i < 255; i++)			//Set all flags to false
+	int index =0;					//Holds last mouse click if no delays
+	clock_t timer = clock();			//Timer to record delays
+	clock_t timer2 = clock();			//Timer to record delays
+
+for (int i = 0; i < 255; i++)	//Set all flags to false
+{
+	flags[i] = false;
+}
+//////////////////////////////////////////////////////MOUSE POS////////////////////////////////////////////////////////////////////////
+	while (!flags[VK_PRIOR])				//Keep going until PgUp key is pressed
 	{
-		flags[i] = false;
-	}
-	
-	{	
-		while (!flags[VK_SCROLL])								//Keep going until scroll key is pressed
-		{		
-			if (clock() % 20 >= 0 && clock() % 20 <= 3)
-				mousePos();
-			for (int key = 8; key <= 255; key++)
+		if(noKey == true)				//Not recording key delays don't check mouse positions
+		{ }
+		else						//Record mouse position 
+		{
+			if (noMouse == false)   		//Record mouse movement
 			{
-		
-				if (GetAsyncKeyState(key) == -32767 && flags[key] == false)		//If the key is pressed down and hasn't been flagged
-					{	
-					flags[key] = true;
-					timer = clock();
-					if (mode == 0)                                                  //Record without delays
+				if (clock() % 20 == 0)
+				{
+					if (tic == false)	//flag is set ignore
 					{
-						delay = 0;
+						mousePos(tic, noKey, noMouse, actions, index);
 					}
-					else
-					{
-						delay = (int)(timer - timer2);
-					}
-					delay = (int)((timer-timer2) / (CLOCKS_PER_SEC / 1000));
-					timer2 = clock();
-					actions.push_back(delay);
-					actions.push_back(key);
-					cout << delay <<"\t"<< key << endl;
-			
-					}
-				else if (GetAsyncKeyState(key) == 0 && flags[key] == true)		//If the key is released after being flagged 
-					{
-					timer = clock();
-					if (mode == 0)
-					{
-						delay = 0;
-					}
-					else
-					{
-						delay = (int)(timer - timer2);
-					}
-					delay = (int)((timer-timer2) / (CLOCKS_PER_SEC / 1000));
-					actions.push_back(delay);
-					actions.push_back(key + 1000);
-					flags[key] = false;
-					cout << delay <<"\t"<< key+1000 << endl;
-					timer2 = clock();
-					}
-			}	
+					else {}			//Already checked this tic
+				}
+				else
+				{
+					tic = false;		//Reset flag to record in next tic
+				}
+			}
+			else {}					//Don't record mouse movement	
 		}
-	}
+//////////////////////////////////////////////////////ACTIONS/////////////////////////////////////////////////////////////////////////
+		for (int key = 1; key <= 255; key++)
+		{
+//////////////////////////////////////////////////////KEY  DOWN///////////////////////////////////////////////////////////////////////
+			if (GetAsyncKeyState(key) == -32767 && flags[key] == false)		//If the key is pressed down and hasn't been flagged
+			{	
+				flags[key] = true;
+				timer = clock();											
+				if (noKey == true)
+				{
+					delay = 2;
+				}
+				else
+				{
+					delay = (int)(timer - timer2);
+				}
+				timer2 = clock();											
+				actions.push_back(delay);
+				actions.push_back(key);
+				cout << delay << "  milliseconds" << "\t" << key << endl;
+				if (key == 1  && noKey == true)               //If Left Click or Right Click are the input and no delays are wanted 
+				{
+					mousePos(tic, noKey, noMouse, actions, index);
+				}
+			}
+///////////////////////////////////////////////////////////////////////////////////KEY UP////////////////////////////////////////////////////////////////////////////////////////////////			
+			else if (GetAsyncKeyState(key) == 0 && flags[key] == true)		//If the key is released after being flagged
+			{
+				timer = clock();												
+				if (noKey == true)
+				{
+					delay = 2;
+				}
+				else
+				{
+					delay = (int)(timer - timer2);
+				}
+				actions.push_back(delay);
+				actions.push_back(key + 1000);
+				flags[key] = false;
+				cout << delay << "  milliseconds" << "\t" << key + 1000 << endl;
+				timer2 = clock();											
+			}
+		}
+	}		
+	
+	
 }
 
 void RecordMacro::saveMacro()
@@ -95,38 +121,36 @@ void RecordMacro::saveMacro()
 	outputFile.close();
 }
 
-void RecordMacro::mousePos()
+void mousePos(bool tic, bool noKey, bool noMouse, vector <int> actions,int& index)
 {
-	vector<long> mousex;
-	vector<long> mousey;
 	POINT point;
-	long delay = 0;
-	if (clock() % 19 == 0)
-	{
-		Sleep(1);
+	long delay=0;
+
 		GetCursorPos(&point);
-		if (mousex.empty() || point.x != mousex[mousex.size() - 2])
+		if (noKey == true)    //Change mouse position delays to when click occurs
 		{
-			delay += 20;
+			delay = actions.size() - index;
 			mousex.push_back(point.x);
 			mousex.push_back(delay);
 			mousey.push_back(point.y);
 			mousey.push_back(delay);
-			delay = 0;
+			index = actions.size();
 		}
-		else
+		else if (noKey == false) //Record normally
 		{
-			delay += 20;
+			if (mousex.empty() || point.x != mousex[mousex.size() - 2])
+			{
+				delay += 20;
+				mousex.push_back(point.x);
+				mousex.push_back(delay);
+				mousey.push_back(point.y);
+				mousey.push_back(delay);
+				delay = 0;
+			}
+			else
+			{
+				delay += 20;
+			}
 		}
-	}
 }
 
-RecordMacro::pause()
-{
-	
-}
-
-RecordMacro::resume()
-{
-	
-}
