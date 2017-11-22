ad738fd2b1f58fbeabcc54ec7543e7212161b006
Nitesh sabankar <niteshsabankar@gmail.com>
2017-11-21 23:44:21 -0800

    updated loadMacro and recordMacro

--- EditMacro.cpp
+++ EditMacro.cpp
@@ -28,95 +28,235 @@ void RecordMacro::saveMacro()
 
 void EditMacro::loadMacro(wstring file_name)
 {
-	//open a file in read mode
-	//read an integer with >>, place it in the actions vector
-	//read a comma, do nothing
-	//repeat until eof
-	
-	ifstream infile;
-	infile.open(file_name);
-	int i;
+	cout << "in load macro" << endl;
+	fstream file;
+	file.open(name.c_str());
 
-	string str((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());  // copy file contents in string
-	
-	stringstream ss(str);
-
-	while (ss >> i)
+	if (!file)	//Error file does not exist exit program
 	{
-		actions.push_back(i);
+		cout << name << " File not found" << endl;
+		exit(1);
+	}
+	else
+	{
+		//-------Local variables------------------------------------------------------------------------
+		string context;      //Holds one content of the file
+		string tmp_num;      //Holds one char of the file
+		int flag;            //Flag for which vector to input
+		int num;             //Convet string input to int
 
-		if (ss.peek() == ',')			// check for comma
-			ss.ignore();
+		string::size_type sz;
+
+		while (file >> context)
+		{
+			if (context == "K")  //Flag one key stroke input
+			{
+				flag = 1;
+				file >> context;
+			}
+
+			else if (context == "MX")  //Flag 2 mouse X point
+			{
+				flag = 2;
+				file >> context;
+			}
+
+			else if (context == "MY")  //Flag 3 mouse Y point
+			{
+				flag = 3;
+				file >> context;
+			}
+
+			//For go throuht each char in the file if char add to context if "," add to vector
+			//--------------------------------------------------------------------
+			for (int i = 0; i < context.length(); i++)
+			{
+				if (context[i] != ',')
+					tmp_num += context[i];
+
+				else if (context[i] == ',')
+				{
+					num = stoi(tmp_num, &sz);    // number base 16
+
+					if (flag == 1)
+						actions.push_back(num);
+
+					else if (flag == 2)
+						mousex.push_back((long)num);
+
+					else if (flag == 3)
+						mousey.push_back((long)num);
+
+					tmp_num = "\0";   //Restart context
+				}
+			}
+		}
 	}
 
-	infile.close();
+	file.close();  //Close file
 }
 
 void RecordMacro::recordMacro(int mode, int vectIndex)
 {
 	bool flags[256];				//Flag for every key
+	bool tic = false;				//Flag to check if MousePos happened in clock tick
+	bool noMouse = false;				//If user doesn't want Mouse positions
+	bool noKey = false;				//If user doesn't want Key delays
 	int delay = 0;					//Time delay between key events
-	clock_t timer = clock();			//
-	clock_t timer2 = clock();			//
+	int index = 0;					//Holds last mouse click if no delays
+	clock_t timer = clock();			//Timer to record delays
+	clock_t timer2 = clock();			//Timer to record delays
 
-	for (int i = 0; i < 255; i++)			//Set all flags to false
+	for (int i = 0; i < 255; i++)	//Set all flags to false
 	{
 		flags[i] = false;
 	}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copy vector items after given vector index in a temporary vector.
+	for (std::vector<int>::const_iterator i = actions.begin() + vectIndex; i != actions.end(); ++i)
+		tempActions.push_back(*i);
 
+	for (std::vector<int>::const_iterator i = mousex.begin() + vectIndex; i != mousex.end(); ++i)
+		tempMousex.push_back(*i);
+
+	for (std::vector<int>::const_iterator i = mousey.begin() + vectIndex; i != mousey.end(); ++i)
+		tempMousey.push_back(*i);
+
+// delete vector items which comes after given vector index
+	actions.resize(vectIndex);
+
+	mousex.resize(vectIndex);
+
+	mousey.resize(vectIndex);
+//////////////////////////////////////////////////////MOUSE POS///////////////////////////////////////////////////////////////
+	while (!flags[VK_PRIOR])				//Keep going until PgUp key is pressed
 	{
-		while (!flags[VK_PRIOR])								//Keep going until scroll key is pressed
+		if (noKey == true)				//Not recording key delays don't check mouse positions
 		{
-			if (clock() % 20 >= 0 && clock() % 20 <= 3)
-			//	mousePos();
-			for (int key = 8; key <= 255; key++)
+		}
+		else						//Record mouse position
+		{
+			if (noMouse == false)   		//Record mouse movement
 			{
-
-				if (GetAsyncKeyState(key) == -32767 && flags[key] == false)		//If the key is pressed down and hasn't been flagged
+				if (clock() % 20 == 0)
 				{
-					flags[key] = true;
-					timer = clock();
-					if (mode == 0)                                                  //Record without delays
+					if (tic == false)	//flag is set ignore
 					{
-						delay = 0;
+						mousePos(tic, noKey, noMouse, actions, index);					// Need to change vectIndex
 					}
-					else
-					{
-						delay = (int)(timer - timer2);
-					}
-					delay = (int)((timer - timer2) / (CLOCKS_PER_SEC / 1000));
-					timer2 = clock();
-					actions.insert(actions.begin() + vectIndex, delay);
-					vectIndex++;
-					actions.insert(actions.begin() + vectIndex, key);
-					vectIndex++;
-					cout << delay << "\t" << key << endl;
-
+					else {}			//Already checked this tic
 				}
-				else if (GetAsyncKeyState(key) == 0 && flags[key] == true)		//If the key is released after being flagged 
+				else
 				{
-					timer = clock();
-					if (mode == 0)
-					{
-						delay = 0;
-					}
-					else
-					{
-						delay = (int)(timer - timer2);
-					}
-					delay = (int)((timer - timer2) / (CLOCKS_PER_SEC / 1000));
-					actions.insert(actions.begin() + vectIndex, delay);
-					vectIndex++;
-					actions.insert(actions.begin() + vectIndex, key + 1000);
-					vectIndex++;
-					flags[key] = false;
-					cout << delay << "\t" << key + 1000 << endl;
-					timer2 = clock();
+					tic = false;		//Reset flag to record in next tic
 				}
+			}
+			else {}					//Don't record mouse movement
+		}
+		//////////////////////////////////////////////////////ACTIONS/////////////////////////////////////////////////////////////////////////
+		for (int key = 1; key <= 255; key++)
+		{
+			//////////////////////////////////////////////////////KEY  DOWN///////////////////////////////////////////////////////////////////////
+			if (GetAsyncKeyState(key) == -32767 && flags[key] == false)		//If the key is pressed down and hasn't been flagged
+			{
+				flags[key] = true;
+				timer = clock();
+				if (noKey == true)
+				{
+					delay = 2;
+				}
+				else
+				{
+					delay = (int)(timer - timer2);
+				}
+				timer2 = clock();
+				actions.push_back(delay);
+				actions.push_back(key);
+				cout << delay << "  milliseconds" << "\t" << key << endl;
+				if (key == 1 && noKey == true)               //If Left Click or Right Click are the input and no delays are wanted
+				{
+					mousePos(tic, noKey, noMouse, actions, index);							// Need to change vectIndex
+				}
+			}
+			////////////////////////////////////////////////////////KEY UP////////////////////////////////////////////////////////////////////////
+			else if (GetAsyncKeyState(key) == 0 && flags[key] == true)		//If the key is released after being flagged
+			{
+				timer = clock();
+				if (noKey == true)
+				{
+					delay = 2;
+				}
+				else
+				{
+					delay = (int)(timer - timer2);
+				}
+				actions.push_back(delay);
+				actions.push_back(key + 1000);
+				flags[key] = false;
+				cout << delay << "  milliseconds" << "\t" << key + 1000 << endl;
+				timer2 = clock();
 			}
 		}
 	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+// push back temporary vector data into main vector 
+	for (std::vector<int>::const_iterator i = tempActions.begin(); i != tempActions.end(); ++i)
+		actions.push_back(*i);
+
+	tempActions.clear(); // clear temporary vector
+
+	for (std::vector<int>::const_iterator i = tempMousex.begin(); i != tempMousex.end(); ++i)
+		mousex.push_back(*i);
+
+	tempMousex.clear();
+
+	for (std::vector<int>::const_iterator i = tempMousey.begin(); i != tempMousey.end(); ++i)
+		mousey.push_back(*i);
+
+	tempMousey.clear();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
+
+
+void mousePos(bool tic, bool noKey, bool noMouse, vector <int> actions, int& index)
+{
+	POINT point;
+	long delay = 0;
+
+
+	GetCursorPos(&point);
+	if (noKey == true)    //Change mouse position delays to when click occurs
+	{
+		delay = actions.size() - index;
+		mousex.push_back(point.x);
+		mousex.push_back(delay);
+		mousey.push_back(point.y);
+		mousey.push_back(delay);
+		
+		index = actions.size();
+	}
+	else if (noKey == false) //Record normally
+	{
+		if (mousex.empty() || point.x != mousex[mousex.size() - 2])
+		{
+			delay += 20;
+			mousex.push_back(point.x);
+			mousex.push_back(delay);
+			mousey.push_back(point.y);
+			mousey.push_back(delay);
+
+			delay = 0;
+		}
+		else
+		{
+			delay += 20;
+		}
+	}
+}
+
+
 void EditMacro::recordSingle(int mode, int delay, int vectIndex)
 {
 	//record a single key down key up. append to actions vector
