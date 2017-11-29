f5c0f26467a5c214735ec321d31f57795f15d597
ZerglingOne <jholcomb87@gmail.com>
2017-11-28 17:26:49 -0800

    AutoMacron UI by Jonathan Holcomb

--- automacronui.cpp
+++ automacronui.cpp
@@ -17,12 +17,12 @@ QStringList keyboardAc, mouseAc;
 QPushButton *clickMe;
 QString fileName;
 
-
+bool set = true;
 bool delays = true;
 bool positions = true;
 bool recording = false;
-int edit_mode = 0, cleaner = 0, edit_index;
-QFuture<void> *myThread;
+int edit_mode = 0, cleaner = 0, edit_index, playbackLoops = 1, playbackWait = 0;
+QFuture<void> *myThread, *myThread2;
 
 AutoMacronUI::AutoMacronUI(QWidget *parent) :
     QMainWindow(parent),
@@ -163,6 +163,7 @@ void AutoMacronUI::on_Tabs_currentChanged(int index)
         record->clear();
         edit->clear();
         playback->clear();
+
     }
 
     if(index == 1)
@@ -186,16 +187,22 @@ void AutoMacronUI::on_Tabs_currentChanged(int index)
     {
         ui->PBXtimes->setEnabled(false);
         ui->StartPB->setEnabled(false);
-        ui->LoopPB->setEnabled(false);
         ui->NumberOfLoops->setEnabled(false);
-        ui->NumberOfLoops->setText("1");
-        ui->SpeedInputPB->setEnabled(false);
-        ui->SpeedInputPB->setText("1.0");
+        ui->NumberOfLoops->setValue(1);
         ui->KeyboardListPB->setEnabled(false);
         ui->MouseListPB->setEnabled(false);
+        ui->delabel->setEnabled(false);
+        ui->delayBox->setEnabled(false);
+        ui->delayBox->setValue(2000);
         record->clear();
         edit->clear();
         playback->clear();
+        keyboardAc.clear();
+        mouseAc.clear();
+        kView->setStringList(keyboardAc);
+        mView->setStringList(mouseAc);
+        ui->KeyboardListPB->update();
+        ui->MouseListPB->update();
     }
 
 }
@@ -230,7 +237,9 @@ void AutoMacronUI::on_RecordMacroEditW_clicked()
         pgup = NULL;
         clickMe = NULL;
         recording = false;
-
+        myThread->waitForFinished();
+        delete myThread;
+        myThread = NULL;
         vector<string> keys, moux, mouy;
         keys = edit->displayMacro();
         moux = edit->mouseInfo('x');
@@ -238,6 +247,10 @@ void AutoMacronUI::on_RecordMacroEditW_clicked()
         set_lists(ui->KeyboardListEditW,
                   ui->MouseListEditW,
                   keys, moux, mouy);
+        keys.clear();
+        moux.clear();
+        mouy.clear();
+        edit_index = 0;
     }
     else
     {
@@ -253,11 +266,18 @@ void AutoMacronUI::on_RecordMacroEditW_clicked()
         scrolls = new QShortcut (QKeySequence(Qt::Key_ScrollLock), clickMe, SLOT(click()));
         pgup = new QShortcut (QKeySequence(Qt::Key_PageUp), clickMe, SLOT(click()));
         if(edit->actionSize() == 0)
-                myThread = new QFuture<void>(QtConcurrent::run(edit, &EditMacro::recordMacro, delays, positions, edit_index));
-        else if(edit_index == 0 && edit->actionSize() != 0)
+                myThread = new QFuture<void>(QtConcurrent::run(edit, &EditMacro::recordMacro, delays, positions, 0, 0, cleaner));
+        else if(edit_index == 0 && edit->actionSize() != 0) //append to the vector
         {
             edit_index = edit->actionSize() - 1;
-            myThread = new QFuture<void>(QtConcurrent::run(edit, &EditMacro::recordMacro, delays, positions, edit_index));
+            int mouseIndex = edit->mouseSize() - 1;
+            myThread = new QFuture<void>(QtConcurrent::run(edit, &EditMacro::recordMacro, delays, positions, edit_index, mouseIndex, cleaner));
+        }
+        else if(edit_index != 0 && edit->actionSize() != 0 && edit_index != edit->actionSize()-1)
+        {
+            int mouseIndex;
+            mouseIndex = edit->matchDelay(edit_index);
+            myThread = new QFuture<void>(QtConcurrent::run(edit, &EditMacro::recordMacro, delays, positions, edit_index, mouseIndex, cleaner));
         }
     }
 }
@@ -309,9 +329,11 @@ void AutoMacronUI::on_LoadMacroEditW_clicked()
     {
         edit_mode = 2;
         edit->loadMacro(fileName.toStdString());
+        ui->NewMacroEditW->setEnabled(false);
         ui->RecordMacroEditW->setEnabled(true);
         ui->KeyboardListEditW->setEnabled(true);
         ui->MouseListEditW->setEnabled(true);
+        ui->DiscardEditW->setEnabled(true);
         vector<string> keys, moux, mouy;
         keys = edit->displayMacro();
         moux = edit->mouseInfo('x');
@@ -319,14 +341,15 @@ void AutoMacronUI::on_LoadMacroEditW_clicked()
         set_lists(ui->KeyboardListEditW,
                   ui->MouseListEditW,
                   keys, moux, mouy);
+        keys.clear();
+        moux.clear();
+        mouy.clear();
 
     }
 }
 
 void AutoMacronUI::on_DiscardEditW_clicked()
 {
-    if(edit_mode == 1)
-    {
         ui->SaveEditW->setEnabled(false);
         ui->RecordMacroEditW->setEnabled(false);
         ui->NewMacroEditW->setEnabled(true);
@@ -339,36 +362,34 @@ void AutoMacronUI::on_DiscardEditW_clicked()
         ui->KeyboardListEditW->update();
         ui->MouseListEditW->update();
         edit->clear();
-    }
-    if(edit_mode == 2)
-       {
-            ui->SaveEditW->setEnabled(false);
-            ui->RecordMacroEditW->setEnabled(false);
-            ui->NewMacroEditW->setEnabled(true);
-            ui->LoadMacroEditW->setEnabled(true);
-            ui->DiscardEditW->setEnabled(false);
-            kView->setStringList(keyboardAc);
-            mView->setStringList(mouseAc);
-            ui->KeyboardListEditW->update();
-            ui->MouseListEditW->update();
-       }
 }
 
 void AutoMacronUI::on_LoadMacroPB_clicked()
 {
+    playback->clear();
     fileName = "";
     fileName = QFileDialog::getOpenFileName(this, tr("Open Your Macro"), tr("/macros/"), tr("Macro Files (*.mcr)"));
     if(fileName != "")
     {
-        ui->PBXtimes->setEnabled(true);
+        //ui->PBXtimes->setEnabled(true);
         ui->StartPB->setEnabled(true);
-        ui->LoopPB->setEnabled(true);
-        ui->NumberOfLoops->setEnabled(true);
-        ui->NumberOfLoops->setText("1");
-        ui->SpeedInputPB->setEnabled(true);
-        ui->SpeedInputPB->setText("1.0");
+        //ui->NumberOfLoops->setEnabled(true);
+        ui->NumberOfLoops->setValue(1);
+
         ui->KeyboardListPB->setEnabled(true);
         ui->MouseListPB->setEnabled(true);
+        ui->delabel->setEnabled(true);
+        ui->delayBox->setEnabled(true);
+        //ui->NumberOfLoops->setEnabled(true);
+        playback->loadMacro(fileName.toStdString());
+        vector<string> keys, moux, mouy;
+        keys = playback->displayMacro();
+        moux = playback->mouseInfo('x');
+        mouy = playback->mouseInfo('y');
+        set_lists(ui->KeyboardListPB,
+                  ui->MouseListPB,
+                  keys, moux, mouy);
+
     }
 }
 
@@ -376,56 +397,32 @@ void AutoMacronUI::on_LoadMacroPB_clicked()
 
 
 void AutoMacronUI::on_StartPB_clicked()
-{
-    ui->LoadMacroPB->setEnabled(false);
-    ui->PBXtimes->setEnabled(false);
-    ui->StartPB->setEnabled(false);
-    ui->LoopPB->setEnabled(false);
-    ui->NumberOfLoops->setEnabled(false);
-    ui->SpeedInputPB->setEnabled(false);
-    ui->StopPB->setEnabled(true);
+{    
+    QTimer *time = new QTimer(this);
+    set = false;
+    ui->delayBox->setEnabled(set);
+    ui->StartPB->setEnabled(set);
+    ui->LoadMacroPB->setEnabled(set);
+   //ui->PBXtimes->setEnabled(set);
+   //ui->NumberOfLoops->setEnabled(set);
+    connect(time, SIGNAL(timeout()), this, SLOT(activateUI()));
+    myThread = new QFuture<void>(QtConcurrent::run(playback, &PlaybackMacro::actionLoop, playbackWait, 1, cleaner));
+    set = true;
+    time->start(playback->longestDelay() + 10 + playbackWait);
 }
 
-
-
-void AutoMacronUI::on_StopPB_clicked()
+/*void AutoMacronUI::on_PBXtimes_clicked()
 {
-    ui->LoadMacroPB->setEnabled(false);
-    ui->PBXtimes->setEnabled(true);
-    ui->StartPB->setEnabled(true);
-    ui->LoopPB->setEnabled(true);
-    ui->NumberOfLoops->setEnabled(true);
-    ui->SpeedInputPB->setEnabled(true);
-    ui->StopPB->setEnabled(false);
-    ui->LoadMacroPB->setEnabled(true);
-}
+    QTimer *time = new QTimer(this);
+    playbackLoops = ui->NumberOfLoops->value();
+    myThread = new QFuture<void>(QtConcurrent::run(playback, &PlaybackMacro::actionLoop, playbackWait, playbackLoops, cleaner));
+    set = false;
+    SLOT(activateUI());
 
+    connect(time, SIGNAL(timeout()), this, SLOT(activateUI()));
+    time->start((playback->longestDelay() * (playbackLoops + 5)) + 5 + playbackWait);
+}*/
 
-void AutoMacronUI::on_PBXtimes_clicked()
-{
-    ui->LoadMacroPB->setEnabled(false);
-    int loops;
-    loops = (ui->NumberOfLoops->text().toInt());
-    ui->PBXtimes->setEnabled(false);
-    ui->StartPB->setEnabled(false);
-    ui->LoopPB->setEnabled(false);
-    ui->NumberOfLoops->setEnabled(false);
-    ui->SpeedInputPB->setEnabled(false);
-    ui->StopPB->setEnabled(true);
-}
-
-
-
-void AutoMacronUI::on_LoopPB_clicked()
-{
-    ui->LoadMacroPB->setEnabled(false);
-    ui->PBXtimes->setEnabled(false);
-    ui->StartPB->setEnabled(false);
-    ui->LoopPB->setEnabled(false);
-    ui->NumberOfLoops->setEnabled(false);
-    ui->SpeedInputPB->setEnabled(false);
-    ui->StopPB->setEnabled(true);
-}
 
 void AutoMacronUI::set_lists(QListView* key, QListView* mouse, vector<string> keybo, vector<string>mouX, vector<string> mouY)
 {
@@ -456,28 +453,33 @@ void AutoMacronUI::set_lists(QListView* key, QListView* mouse, vector<string> ke
 void AutoMacronUI::on_KeyboardListEditW_pressed(const QModelIndex &index)
 {
     edit_index = index.row();
+    cout << edit->matchDelay(edit_index) << endl;
 }
 
 
 void AutoMacronUI::on_KeyboardListEditW_customContextMenuRequested(const QPoint &pos)
 {
-    QAction *ibef, *iaft, *cdel;
+    QAction *ibef, *iaft, *cdel, *rema;
     ibef = new QAction("Insert Action Before", this);
     iaft = new QAction("Insert Action After", this);
     cdel = new QAction("Change Delay", this);
+    rema = new QAction("Remove delay + Action", this);
     ibef->setObjectName("ibef");
     iaft->setObjectName("iaft");
     cdel->setObjectName("cdel");
+    rema->setObjectName("rema");
     QMenu *menu=new QMenu(this);
 
     menu->addAction(ibef);
     menu->addAction(iaft);
     menu->addAction(cdel);
+    menu->addAction(rema);
     menu->popup(mapToGlobal(pos));
 
     connect(ibef, SIGNAL(triggered(bool)), this, SLOT(ibef_triggered(bool)));
     connect(iaft, SIGNAL(triggered(bool)), this, SLOT(iaft_triggered(bool)));
     connect(cdel, SIGNAL(triggered(bool)), this, SLOT(cdel_triggered(bool)));
+    connect(rema, SIGNAL(triggered(bool)), this, SLOT(rema_triggered(bool)));
 }
 
 void AutoMacronUI::on_RWDEdit_toggled(bool checked)
@@ -505,6 +507,9 @@ void AutoMacronUI::ibef_triggered(bool checked)
     set_lists(ui->KeyboardListEditW,
               ui->MouseListEditW,
               keys, moux, mouy);
+    keys.clear();
+    moux.clear();
+    mouy.clear();
 }
 
 void AutoMacronUI::iaft_triggered(bool checked)
@@ -527,18 +532,21 @@ void AutoMacronUI::iaft_triggered(bool checked)
     set_lists(ui->KeyboardListEditW,
               ui->MouseListEditW,
               keys, moux, mouy);
+    keys.clear();
+    moux.clear();
+    mouy.clear();
 }
 
 void AutoMacronUI::cdel_triggered(bool checked)
 {
-    int temp, delay;
+    int temp, delay = -1;
      if(edit_index%2 != 0)
          temp = edit_index - 1;
      else
          temp = edit_index;
      delay = QInputDialog::getInt(this, tr("Enter Delay"),tr("delay"), QLineEdit::Normal);
-     cout << temp << endl;
-     if(delay != 0)
+
+     if(delay != -1)
      {
 
          edit->changeDelay(temp, delay);
@@ -550,11 +558,51 @@ void AutoMacronUI::cdel_triggered(bool checked)
          set_lists(ui->KeyboardListEditW,
                ui->MouseListEditW,
                keys, moux, mouy);
+         keys = edit->displayMacro();
+         moux = edit->mouseInfo('x');
+         mouy = edit->mouseInfo('y');
+         ui->SaveEditW->setEnabled(true);
      }
 
+}
+
+void AutoMacronUI::rema_triggered(bool checked)
+{
+    edit->removeAction(edit_index);
+    vector<string> keys, moux, mouy;
+    keys = edit->displayMacro();
+    moux = edit->mouseInfo('x');
+    mouy = edit->mouseInfo('y');
+    set_lists(ui->KeyboardListEditW,
+          ui->MouseListEditW,
+          keys, moux, mouy);
+    keys = edit->displayMacro();
+    moux = edit->mouseInfo('x');
+    mouy = edit->mouseInfo('y');
+    ui->SaveEditW->setEnabled(true);
 }
 
 void AutoMacronUI::on_RMPEdit_toggled(bool checked)
 {
     positions = checked;
+}
+
+
+void AutoMacronUI::on_delayBox_valueChanged(int arg1)
+{
+    playbackWait = arg1;
+}
+
+void AutoMacronUI::on_NumberOfLoops_valueChanged(int arg1)
+{
+    playbackLoops = arg1;
+}
+
+void AutoMacronUI::activateUI()
+{
+    ui->delayBox->setEnabled(set);
+    ui->StartPB->setEnabled(set);
+    ui->LoadMacroPB->setEnabled(set);
+   //ui->PBXtimes->setEnabled(set);
+   //ui->NumberOfLoops->setEnabled(set);
 }
