9a0fb2643dfc2282c196a16c9e762386426cb65b
ZerglingOne <jholcomb87@gmail.com>
2017-11-28 17:17:37 -0800

    EditMacro By Nitesh Sabankar

--- EditMacro.h
+++ EditMacro.h
@@ -2,15 +2,24 @@
 #define EDITMACRO_H
 
 #include <Macro.h>
+#include <windows.h>
+#include <time.h>
+#include <cstdio>
+#include <iostream>
+#include <conio.h>
+using namespace std;
 
 class EditMacro : public Macro
 {
-	public:
-		void saveMacro();
-		void loadMacro(wstring);
-		void recordSingle(int);
-		void record(int);
-		void pause();
-		void resume();
-		
+        public:
+                EditMacro();
+                void saveMacro(string);
+                void loadMacro(string);
+                void recordSingle(bool, int);
+                void recordMacro(bool, bool, int, int, int);
+                void removeAction(int&);
+                void changeDelay(int&, int&);
+                void mousePos(bool, bool, bool, vector<int>, int&);
 };
+
+#endif
